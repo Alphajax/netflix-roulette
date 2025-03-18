a627c98be7156ab5efb14f76378676c1936a36ca
Dzmitry Chervanenka <dzmitry_chervanenka@epam.com>
2025-03-18 17:40:11 +0300

    refactor: refactor counter.ts component

--- src/components/counter/counter.ts
+++ src/components/counter/counter.ts
@@ -14,39 +14,41 @@ export class Counter extends Component<Props, State> {
 
     handleDecrementButtonClick = () => {
         this.setState(prev => ({
-            ...prev,
             value: prev.value - 1,
         }));
     };
 
     handleIncrementButtonClick = () => {
         this.setState(prev => ({
-            ...prev,
             value: prev.value + 1,
         }));
     };
 
     render() {
+        const incrementButton = createElement(
+            "button",
+            {
+                className: clsx(styles.button, styles.primary),
+                onClick: this.handleIncrementButtonClick,
+            },
+            "Increment",
+        );
+
+        const decrementButton = createElement(
+            "button",
+            {
+                className: clsx(styles.button, styles.cancel),
+                onClick: this.handleDecrementButtonClick,
+            },
+            "Decrement",
+        );
+
         return createElement(
             "div",
             { className: styles.counter },
             createElement("h1", null, this.state.value),
-            createElement(
-                "button",
-                {
-                    className: clsx(styles.button, styles.primary),
-                    onClick: this.handleIncrementButtonClick,
-                },
-                "Increment",
-            ),
-            createElement(
-                "button",
-                {
-                    className: clsx(styles.button, styles.cancel),
-                    onClick: this.handleDecrementButtonClick,
-                },
-                "Decrement",
-            ),
+            incrementButton,
+            decrementButton,
         );
     }
 }
