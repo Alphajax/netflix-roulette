944dbf9edc3fe91c0dc7611894ab98b539bad698
Dzmitry Chervanenka <dzmitry_chervanenka@epam.com>
2025-03-18 18:55:44 +0300

    feat: implement search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useCallback } from "react";
+
 import { Counter } from "./components";
+import { Search } from "./ui";
 
 import "./App.css";
 
 function App() {
-  return <Counter initialValue={7}/>;
+    const onSearch = useCallback((search: string) => {
+        alert("search by value:" + search)
+    }, []);
+
+    return (
+        <>
+            <Counter initialValue={7} />
+            <Search initialSearch={"netflix"} onSearch={onSearch} />
+        </>
+    );
 }
 
 export default App;
---
+++ src/ui/search/search.tsx
@@ -0,0 +1,47 @@
+import clsx from "clsx";
+import { ChangeEvent, KeyboardEvent, MouseEvent, useState } from "react";
+
+import { KEY_CODE } from "../../constants";
+import { Button } from "../button";
+
+import styles from "./search.module.scss";
+
+type Props = {
+    initialSearch: string;
+    onSearch: (search: string) => void;
+};
+
+export const Search = ({ initialSearch, onSearch }: Props) => {
+    const [search, setSearch] = useState(initialSearch);
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    };
+
+    const handleSearchClick = (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        onSearch(search);
+    };
+
+    const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        e.preventDefault();
+
+        if (e.key === KEY_CODE.ENTER) {
+            onSearch(search);
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <input
+                onKeyDown={handleKeyPress}
+                type="text"
+                defaultValue={initialSearch}
+                onChange={handleInputChange}
+                className={clsx(styles.searchInput)}
+                value={search}
+            />
+            <Button onClick={handleSearchClick}>Search</Button>
+        </div>
+    );
+};
